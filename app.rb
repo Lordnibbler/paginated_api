adb2f109632ed57826ea859f43e86162c3482a34
Ben Radler <ben@benradler.com>
2020-03-31 15:26:37 -0700

    use JSON

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
+require 'json'
+
+before do
+  content_type :json
+end
 
 get '/results/:page' do
   page = params[:page].to_i
-  return {
+  {
     page: page,
-    results: (page - 1...page * 10),
-  }
+    results: (page - 1...page * 10).to_a,
+  }.to_json
 end
